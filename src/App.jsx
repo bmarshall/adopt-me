9240dc77e66d6e9eea09ef327f223fdab5afde7d
Brian Marshall <brian@brians-mbp.lan>
2022-12-29 21:12:27 -0600

    Server Side Render code along

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,43 @@
-import { useState } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { useState, lazy, Suspense } from "react";
+import { Routes, Route, Link } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import SearchParams from "./SearchParams";
-import Details from "./Details";
 import AdoptedPetContext from "./AdoptedPetContext";
+
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
 
 const queryClient = new QueryClient({
   defaultOptions: {
-    queries : {
+    queries: {
       staleTime: Infinity,
-      cacheTime: Infinity
-    }
-  }
+      cacheTime: Infinity,
+    },
+  },
 });
 
 const App = () => {
   const adoptedPet = useState(null);
   return (
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <AdoptedPetContext.Provider value={adoptedPet}>
+    <QueryClientProvider client={queryClient}>
+      <AdoptedPetContext.Provider value={adoptedPet}>
+        <Suspense
+          fallback={
+            <div className="loading-pane">
+              <h2 className="loader">🐶</h2>
+            </div>
+          }
+        >
           <header>
             <Link to="/">Adopt Me!</Link>
           </header>
           <Routes>
-            <Route path="/details/:id" element={<Details/>}/>
-            <Route path="/" element={<SearchParams/>}/>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
           </Routes>
-        </AdoptedPetContext.Provider>
-      </QueryClientProvider> 
-    </BrowserRouter>
+        </Suspense>
+      </AdoptedPetContext.Provider>
+    </QueryClientProvider>
   );
-}
+};
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render((<App/>));
+export default App;
